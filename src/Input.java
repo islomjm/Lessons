8bed0f7380e4addc73e5564b2d97ead227c033aa
islom <islomnizom@gmail.com>
2022-08-13 12:44:11 +0300

    Alishev JavaCore

--- src/Input.java
+++ src/Input.java
@@ -6,7 +6,7 @@ public class Input {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Введите что нибудь");
+        System.out.println("Введите что нибудь:");
         String string = s.nextLine();
         System.out.println("Вы ввели " + string);
         Scanner d = new Scanner(System.in);
